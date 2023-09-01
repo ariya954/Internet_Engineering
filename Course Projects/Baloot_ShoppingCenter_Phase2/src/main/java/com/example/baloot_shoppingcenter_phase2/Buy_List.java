8d00e41fe8141cfa979d79cf291b49f2414af3b9
Mohammad Taghizadeh <ariyataghizadeh@gmail.com>
2023-09-01 20:32:06 +0430

    New Commit

--- Course Projects/Baloot_ShoppingCenter_Phase2/src/main/java/com/example/baloot_shoppingcenter_phase2/Buy_List.java
+++
@@ -1,8 +0,0 @@
-package com.example.baloot_shoppingcenter_phase2;
-
-public class Buy_List {
-    Buy_List(int Commodity_id) {
-        Commodity = Commodity_id;
-    }
-    public int Commodity;
-}
